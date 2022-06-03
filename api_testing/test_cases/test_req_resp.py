1253127ca158433ae907e8c8043071cd3239fc8b
PriyaShah12 <prshah160@gmail.com>
2022-06-03 11:28:01 -0400

    Base page changes
    Added a base page

---
+++ api_testing/test_cases/test_req_resp.py
@@ -0,0 +1,30 @@
+import json
+
+from api_testing.pages.basepage import BasePg
+from configuration.config import TestData
+
+     #practice work
+class Test_Req_Res(BasePg):
+    auth_token = "3b431062780351dd03b6c7a3a773a4a22645ef1c33eab67ec920ec3056e0ef45"
+    header = {'Content_Type': 'application/json'}
+    # 'Accept': 'application/json','Authorization': 'Bearer' + auth_token}
+
+    def test_read_response(self):
+        response = self.get(TestData.URL)
+        print(response.json())
+
+
+    def test_update_resp(self):
+        response = self.get(TestData.URL)
+        value = self.check_resp_code(response, 200)
+        assert value
+
+    def test_element_present(self):
+        response = self.get(TestData.URL)
+        value = self.check_element_in_a_dictionary(response, "name", "Ms. Priyala Guneta")
+        assert value
+
+    def test_header(self):
+        pass
+
+
---
+++ configuration/config.py
@@ -0,0 +1,3 @@
+class TestData:
+
+    URL = "https://gorest.co.in/public/v2/users"
